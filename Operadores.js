662cc48f5ed3becf49e98239be6f297ea496baed
Natali <natalic1008.nm@gmail.com>
2023-01-18 17:06:25 -0500

    operadores

---
+++ Operadores.js
@@ -0,0 +1,15 @@
+console.log(3+5);
+console.log (14-7);
+console.log (4*5);
+console.log (30/9);
+console.log (21%5);
+console.log (3+5*2-8);
+console.log ((3+5)*2-8);
+
+
+console.log("HOLA" +" "+ "NATA");
+console.log ("HOLA" + "NATA");
+
+var nombre = " MARIANA ";
+var saludo = " Hola" + nombre;
+console.log(saludo);
