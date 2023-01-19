2616ce23ad231a934c2f8c7fe4d2366cd01174a7
Natali <natalic1008.nm@gmail.com>
2023-01-19 11:27:31 -0500

    Funciones

---
+++ Funciones.js
@@ -0,0 +1,19 @@
+// function sumaTres(x) { 
+// console.log (x+3)
+// }
+//sumaTres (5)
+
+//PRIMERO
+function sumaTres (x) {
+    return (x + 3)
+}
+
+// SEGUNDA
+var sumaTres = function (x) {
+    return (x +3)
+}
+
+// TERCERA 
+var sumaTres = (x)=> {
+    return (x + 3)
+}
