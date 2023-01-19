afb21fca4c71cae427677481c26572191ad86c0f
Natali <natalic1008.nm@gmail.com>
2023-01-19 18:50:45 -0500

    bucles

---
+++ bucles.js
@@ -0,0 +1,23 @@
+var suma=(0)
+//suma = (suma + 1)
+//suma = (suma + 2)
+//suma = (suma + 3)
+//suma = (suma + 4)
+//console.log(suma)
+
+// for(var i=0 ; i<10 ; i++) [ 
+//    suma = suma + 1; 
+//    console.log(suma)
+//
+
+//for(var i=0 ; i<5; i++){
+//    suma = suma + i
+  //  console.log ("variable de iteracion: ", i)
+//}
+//console.log ("variable Suma: ", suma )
+
+// bucle White
+while(suma < 3){
+    suma = suma + 1
+    console.log(suma)
+}
