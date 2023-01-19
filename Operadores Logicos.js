ceddbf2ff283b0bbc73320c41581f61fe72d1876
Natali <natalic1008.nm@gmail.com>
2023-01-19 18:15:29 -0500

    Operadores logicos

---
+++ Operadores Logicos.js
@@ -0,0 +1,24 @@
+// &&
+// function mayorMenor(num) {
+// if(num >5 && num < 10) console.log (true)
+// else console.log (false)
+// }
+
+//mayorMenor(8)
+
+//function mayorMenorPar(num){
+  //  if (num > 5 && num < 10 && num %2 === 0) console.log (true)
+    //else console.log (false)
+//}
+
+//mayorMenorPar(7)
+//
+//mayorMenorPar(8)
+
+//FUNCION OR
+//function operadorOr( str) {
+  //  if(str === "Henry" || str.length < 2) console.log (true)
+    //else console.log (false)
+//}
+
+// operadorOr ("si")
