805e4197050d1d75806b56cd27136cec355cd664
Natali <natalic1008.nm@gmail.com>
2023-01-20 18:33:33 -0500

    arrays

---
+++ arrays.js
@@ -0,0 +1,34 @@
+//var ListaDeCompras = []
+
+//ListaDeCompras[3]= "Tomate"
+//ListaDeCompras[2] = "Lechuga"
+//ListaDeCompras [1] = "Zanahoria"
+//ListaDeCompras [0] = "Alberja"
+
+// console.log (ListaDeCompras.length);
+
+//var elementosDelArray = ListaDeCompras [2]
+//console.log (elementosDelArray)
+
+//var palabra = "Henri";
+//var pablabraSeparada = palabra.split ("");
+//pablabraSeparada.pop();
+//pablabraSeparada.push("y");
+//console.log (pablabraSeparada);
+//palabraArreglada = pablabraSeparada.join ("");
+//console.log (palabraArreglada);
+
+//var numeros = [1,2,3,4];
+//numeros.forEach ((num)=> console.log (num));
+
+//var arr = [1,2,3,4,5];
+//for (var i=0; i<arr.length; i++){
+//    console.log(arr[i]);
+//
+
+var arr=[]
+while(arr.length<5){
+    arr.push("BOOM")
+}
+
+console.log(arr);
