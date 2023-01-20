460ff274f3bfee8409ed87e19b71b95f924435d8
Natali <natalic1008.nm@gmail.com>
2023-01-20 15:27:37 -0500

    Controlde flujo

---
+++ ControlDeFlijo.js
@@ -0,0 +1,10 @@
+function viajar(destino){
+    if (destino === "Argentina"){
+        console.log ("Girar a la IZQUIERDA")
+    } else if (destino === "Colombia"){
+        console.log ("Giara a la DERECHA")
+    } else {
+    console.log ("Nos perdimos")
+}
+}
+viajar ("Peru")
